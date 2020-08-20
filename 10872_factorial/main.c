58d295d949eccf29b43d55bee1dc7c3be41aab79
Ubuntu <ubuntu@ip-172-26-14-54.ap-northeast-2.compute.internal>
2020-08-20 11:22:49 +0000

    fin_try1

---
+++ 10872_factorial/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int recursive_factorial(int result, int number)
+{
+    if (number - 1 > 0 )
+    {
+        result = recursive_factorial(result, number - 1);
+    }
+    result *= number;
+
+    return result;
+}
+
+int main (void)
+{
+    int number;
+    int result = 1;
+    scanf("%d", &number);
+
+    result = recursive_factorial(result, number);
+    printf("%d\n",result);
+
+    return (0);
+}
